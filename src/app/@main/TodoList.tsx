15f8d4deaa279bd9784b2cfaf5e96d69f8223895
load28 <tjalsdud89@naver.com>
2024-07-08 16:53:29 +0900

    use useMemo in TodoList component

--- src/app/@main/TodoList.tsx
+++ src/app/@main/TodoList.tsx
@@ -1,46 +1,106 @@
-'use client';
+import { Todo } from '@/api/todo';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { ChangeEvent, useState } from 'react';
+import { ActionIcon, Badge, Checkbox, Group, Menu, Stack, Text } from '@mantine/core';
+import classes from '@/app/@main/TodoList.module.css';
+import { IconArrowBack, IconDotsVertical, IconEdit, IconTrash } from '@tabler/icons-react';
 
-import { sortDate } from '@/utils/date';
-import { Stack } from '@mantine/core';
-import { useQuery } from '@tanstack/react-query';
-import classes from './TodoList.module.css';
-import { getTodos, Todo } from '@/api/todo';
-import React from 'react';
-import { TodoListByDate } from '@/app/@main/TodoListByDate';
-
-export function TodoList() {
-  const { data: todos, isLoading } = useQuery({
-    queryKey: ['todos'],
-    queryFn: getTodos,
+export function TodoList({ todos, date }: { todos: Todo[]; date: string }) {
+  const [mode, setMode] = useState<'view' | 'edit'>('view');
+  const queryClient = useQueryClient();
+  const todoMutation = useMutation({
+    mutationKey: ['todos'],
+    mutationFn: async (id: string) => {
+      const allTodos = queryClient.getQueryData<Todo[]>(['todos']) || [];
+      return allTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isComplete: !todo.isComplete,
+          };
+        }
+        return todo;
+      });
+    },
+    onSuccess: (todos) => {
+      queryClient.setQueryData(['todos'], todos);
+    },
   });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const checkboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    todoMutation.mutate(e.target.id);
+  };
 
-  if (!todos) {
-    return <></>;
-  }
-
-  const todoMap = todos.reduce<Record<string, Todo[]>>((acc, todo) => {
-    return {
-      ...acc,
-      [todo.date]: [...(acc[todo.date] || []), todo],
-    };
-  }, {});
-  const dates = Object.keys(todoMap);
-  const sortedDates = sortDate(dates, 'des');
+  const editHandler = () => {
+    setMode('edit');
+  };
 
   return (
-    <Stack gap={32}>
-      {sortedDates.map((date) => {
-        return (
-          <Stack key={date} gap={18}>
-            <TodoListByDate key={date} todos={todoMap[date]} date={date} />
-            <div className={classes.todoDivider} />
-          </Stack>
-        );
-      })}
-    </Stack>
+    <div>
+      <Group className={classes.todoSettingMenu} justify={'space-between'}>
+        <Text size="md" fw={700} c="gray.8">
+          {date}
+        </Text>
+        {mode === 'view' ? (
+          <Menu width={180} styles={{ itemLabel: { fontSize: '12px' } }} position={'bottom-end'}>
+            <Menu.Target>
+              <ActionIcon size={'sm'} color={'gray.6'} variant={'subtle'}>
+                <IconDotsVertical size={16} />
+              </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item leftSection={<IconEdit size={14} />} onClick={editHandler}>
+                Edit
+              </Menu.Item>
+              <Menu.Item leftSection={<IconTrash size={14} />} styles={{ item: { color: 'red' } }}>
+                Delete
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        ) : (
+          <ActionIcon size={20} color={'gray.6'} variant={'subtle'} onClick={() => setMode('view')}>
+            <IconArrowBack />
+          </ActionIcon>
+        )}
+      </Group>
+      <Stack gap={16} pt={16} pb={16} pl={8} pr={8}>
+        {todos.map((todo) => {
+          return (
+            <label key={todo.id} className={classes.todoLabel} htmlFor={todo.id}>
+              <Group align={'flex-start'} wrap={'nowrap'} gap={8} className={classes.todoContainer}>
+                <Checkbox id={todo.id} color={'gray.5'} size={'xs'} pt={2} checked={todo.isComplete} onChange={checkboxHandler} />
+                <Stack gap={12} flex={1}>
+                  <Group justify={'space-between'} align={'flex-start'} wrap={'nowrap'}>
+                    <Text
+                      size={'sm'}
+                      fw={todo.isComplete ? 400 : 500}
+                      c={todo.isComplete ? 'gray.5' : 'gray.8'}
+                      td={todo.isComplete ? 'line-through' : undefined}
+                      fs={todo.isComplete ? 'italic' : undefined}
+                    >
+                      {todo.description}
+                    </Text>
+                  </Group>
+                  <Group gap={8}>
+                    {todo.hashtag.map((tag, index) => {
+                      return (
+                        <Badge key={index} size={'xs'} color={'gray.5'} variant={'outline'}>
+                          {tag}
+                        </Badge>
+                      );
+                    })}
+                  </Group>
+                </Stack>
+                {mode === 'edit' ? (
+                  <ActionIcon size={18} color={'red.4'}>
+                    <IconTrash />
+                  </ActionIcon>
+                ) : undefined}
+              </Group>
+            </label>
+          );
+        })}
+      </Stack>
+    </div>
   );
 }
--- src/app/@main/TodoPage.tsx
+++ src/app/@main/TodoPage.tsx
@@ -1,11 +1,19 @@
 'use client';
 
-import { dehydrate, HydrationBoundary, useQueryClient } from '@tanstack/react-query';
-import { TodoList } from './TodoList';
-import { getTodos } from '@/api/todo';
+import { dehydrate, HydrationBoundary, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getTodos, Todo } from '@/api/todo';
+import { Stack } from '@mantine/core';
+import { TodoList } from '@/app/@main/TodoList';
+import classes from '@/app/@main/TodoPage.module.css';
+import React, { useMemo } from 'react';
+import { sortDate } from '@/utils/date';
 
 export function TodoPage() {
   const client = useQueryClient();
+  const { data: todos, isLoading } = useQuery({
+    queryKey: ['todos'],
+    queryFn: getTodos,
+  });
   const fetchData = async () => {
     await client.prefetchQuery({
       queryKey: ['todos'],
@@ -13,10 +21,39 @@ export function TodoPage() {
     });
     return dehydrate(client);
   };
+  const { todoMap, sortedDates } = useMemo(() => {
+    const todoMap =
+      todos?.reduce<Record<string, Todo[]>>((acc, todo) => {
+        return {
+          ...acc,
+          [todo.date]: [...(acc[todo.date] || []), todo],
+        };
+      }, {}) || {};
+    const dates = Object.keys(todoMap);
+    const sortedDates = sortDate(dates, 'des');
+    return { todoMap, sortedDates };
+  }, [todos]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (sortedDates.length === 0) {
+    return <div>Empty</div>;
+  }
 
   return (
     <HydrationBoundary state={fetchData}>
-      <TodoList />
+      <Stack gap={32}>
+        {sortedDates.map((date) => {
+          return (
+            <Stack key={date} gap={18}>
+              <TodoList key={date} todos={todoMap[date]} date={date} />
+              <div className={classes.todoDivider} />
+            </Stack>
+          );
+        })}
+      </Stack>
     </HydrationBoundary>
   );
 }
