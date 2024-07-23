05ebb5f22a33daa4725b3ed972b70851ed90b5aa
load28 <tjalsdud89@naver.com>
2024-07-23 11:13:20 +0900

    refactor: remove login loading ui

--- src/app/@core/providers/Auth.context.tsx
+++ src/app/@core/providers/Auth.context.tsx
@@ -1,18 +1,16 @@
 'use client';
 
+import Login from '@/app/@login/page';
 import { useSession } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
-import { Text } from '@mantine/core';
-import Login from '@/app/@login/page';
 
 export function AuthProvider({ children }: PropsWithChildren) {
   const session = useSession();
 
   return (
     <>
-      {session?.status === 'loading' && <Text>Loading...</Text>}
-      {session?.status === 'unauthenticated' && <Login />}
-      {session?.status === 'authenticated' && children}
+      { session?.status === 'unauthenticated' && <Login /> }
+      { session?.status === 'authenticated' && children }
     </>
   );
 }
