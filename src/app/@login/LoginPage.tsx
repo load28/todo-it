699b14aa2854d0b83188da5700f0b1a902ffc86a
load28 <tjalsdud89@naver.com>
2024-09-23 10:35:47 +0900

    refactor: add google gsi client type and code refactoring

--- src/app/@login/LoginPage.tsx
+++ src/app/@login/LoginPage.tsx
@@ -4,40 +4,38 @@ import { Stack, Text } from '@mantine/core';
 import Script from 'next/script';
 import { signIn } from 'next-auth/react';
 import React from 'react';
+import { CredentialResponse } from 'google-one-tap';
 
-const handleCredentialResponse = (response) => {
-  fetch('/api/auth/callback/google', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ credential: response.credential }),
-  }).then(() => {
-    signIn('google').then();
-  });
+const handleCredentialResponse = async (response: CredentialResponse) => {
+  try {
+    await fetch('/api/auth/callback/google', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ credential: response.credential }),
+    });
+
+    await signIn('google');
+  } catch (e: unknown) {
+    console.error(e);
+  }
 };
 
 const initializeGoogleSignIn = () => {
-  console.log(window.google);
-  window.google.accounts.id.initialize({
-    client_id: process.env.NEXT_PUBLIC_AUTH_GOOGLE_CLIENT_ID,
-    callback: handleCredentialResponse,
-  });
-  window.google.accounts.id.renderButton(document.getElementById('google-signin-button'), {
-    theme: 'outline',
-    size: 'large',
-  });
+  const targetElement = document.getElementById('google-signin-button');
+  const clientId = process.env.NEXT_PUBLIC_AUTH_GOOGLE_CLIENT_ID;
+
+  if (!(targetElement && clientId)) {
+    return;
+  }
+
+  window.google.accounts.id.initialize({ client_id: clientId, callback: handleCredentialResponse });
+  window.google.accounts.id.renderButton(targetElement, { theme: 'outline', size: 'large' });
 };
 
 export default function LoginPage() {
   return (
     <Stack justify={'center'} align={'center'} m={'auto'} h={'100vh'}>
-      <Script
-        src="https://accounts.google.com/gsi/client"
-        onLoad={() => {
-          if (window.google) {
-            initializeGoogleSignIn();
-          }
-        }}
-      />
+      <Script src="https://accounts.google.com/gsi/client" onLoad={() => initializeGoogleSignIn()} />
       <Text size={'xl'} fw={'bold'}>
         Todo it
       </Text>
