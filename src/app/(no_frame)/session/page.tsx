c2300e503da9c31d22b509ee039260a55484fd80
load28 <tjalsdud89@naver.com>
2024-09-27 14:27:44 +0900

    apply react-query

--- src/app/(no_frame)/session/page.tsx
+++ src/app/(no_frame)/session/page.tsx
@@ -1,55 +1,45 @@
 'use client';
 
 import { Text } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+
+const getUserInfo = async (code: string) => {
+  try {
+    const queryParams = new URLSearchParams({ code }).toString();
+    const res = await fetch(`/api/auth/user/accessToken?${queryParams}`, { method: 'GET' });
+
+    return await res.json();
+  } catch (e) {
+    return null;
+  }
+};
 
 export default function Session() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [isLoad, setIsLoad] = useState(false);
-  const [userData, setUserData] = useState<{ name: string; photoUrl: string; email: string }>();
+  const code = useMemo(() => searchParams.get('code'), [searchParams]);
+  const { data, isSuccess } = useQuery({
+    queryKey: ['signup', 'userInfo', code],
+    queryFn: ({ queryKey }) => getUserInfo(queryKey[2] as string),
+    enabled: !!code,
+  });
 
   useEffect(() => {
-    if (!isLoad) {
-      // TODO 두번 요청하는 문제 해결필요
-      const getUserInfo = async () => {
-        try {
-          const code = searchParams.get('code');
-          if (!code) {
-            router.push('/signup');
-            return;
-          }
-
-          router.replace(window.location.pathname);
-          const queryParams = new URLSearchParams({
-            code,
-          }).toString();
-          const res = await fetch(`/api/auth/user/accessToken?${queryParams}`, { method: 'GET' });
-
-          return await res.json();
-        } catch (e) {
-          return null;
-        }
-      };
-
-      getUserInfo().then((data) => {
-        if (data) {
-          setUserData({ name: data.name, photoUrl: data.picture, email: data.email });
-        }
-      });
-      setIsLoad(true);
+    if (!code) {
+      router.replace('/signup');
     }
-  }, [isLoad]);
+  }, [code]);
 
   return (
     <>
-      {userData && (
+      {isSuccess && (
         <>
-          <Text size={'lg'}>{userData.email}</Text>
-          <Text size={'lg'}>{userData.name}</Text>
-          <Image src={userData.photoUrl} alt={userData.name} width={100} height={100} />
+          <Text size={'lg'}>{data.email}</Text>
+          <Text size={'lg'}>{data.name}</Text>
+          <Image src={data.picture} alt={data.name} width={100} height={100} />
         </>
       )}
     </>
