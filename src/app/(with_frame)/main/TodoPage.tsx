7c589208b209bd3f19ac656906c59fa99891072b
load28 <tjalsdud89@naver.com>
2024-10-03 10:00:18 +0900

    remove unsued code in TodoPage

--- src/app/(with_frame)/main/TodoPage.tsx
+++ src/app/(with_frame)/main/TodoPage.tsx
@@ -1,24 +1,16 @@
 'use client';
 
 import { TodoList } from '@/app/(with_frame)/main/TodoList';
-import { getTodos } from '@/app/api/todo';
 import { sortDate } from '@/core/date';
 import { Divider, Stack } from '@mantine/core';
-import { dehydrate, HydrationBoundary, useQueryClient } from '@tanstack/react-query';
 import React, { useMemo } from 'react';
 import { useTzContext } from '@/app/@core/providers/TimezoneProvider';
 import { useTodoQuery } from '@/app/@core/query/todo-query';
 
 export function TodoPage() {
   const tzCtx = useTzContext();
-  const client = useQueryClient();
   const { data: todoMap, isLoading } = useTodoQuery(tzCtx?.tz);
   const sortedDates = useMemo(() => sortDate(Object.keys(todoMap || {}), 'des'), [todoMap]);
-
-  const fetchData = async () => {
-    await client.prefetchQuery({ queryKey: ['todos'], queryFn: getTodos(tzCtx?.tz) });
-    return dehydrate(client);
-  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -29,18 +21,16 @@ export function TodoPage() {
   }
 
   return (
-    <HydrationBoundary state={fetchData}>
-      <Stack gap={32}>
-        {todoMap &&
-          sortedDates.map((date) => {
-            return (
-              <Stack key={date} gap={18}>
-                <TodoList key={date} todos={todoMap[date]} date={date} />
-                <Divider />
-              </Stack>
-            );
-          })}
-      </Stack>
-    </HydrationBoundary>
+    <Stack gap={32}>
+      {todoMap &&
+        sortedDates.map((date) => {
+          return (
+            <Stack key={date} gap={18}>
+              <TodoList key={date} todos={todoMap[date]} date={date} />
+              <Divider />
+            </Stack>
+          );
+        })}
+    </Stack>
   );
 }
