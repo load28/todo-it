b18d43f64ba347ee7431897cc39ea16255923c8d
load28 <tjalsdud89@naver.com>
2024-11-26 12:49:01 +0900

    change env name

--- src/core/auth/auth-config.ts
+++ src/core/auth/auth-config.ts
@@ -6,7 +6,7 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authConfig = {
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_AUTH_GOOGLE_ID,
+      clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
--- src/core/providers/env/env-server.ts
+++ src/core/providers/env/env-server.ts
@@ -6,7 +6,6 @@ const ENV = {
   AUTH_URL: process.env.AUTH_URL as string,
   AUTH_SECRET: process.env.AUTH_SECRET as string,
   AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET as string,
-  NEXT_PUBLIC_AUTH_GOOGLE_ID: process.env.NEXT_PUBLIC_AUTH_GOOGLE_ID as string,
 } as const;
 
 /**
