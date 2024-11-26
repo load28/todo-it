8095c25accc28443ba38d44a84326ac47a48774d
load28 <tjalsdud89@naver.com>
2024-11-26 10:50:41 +0900

    change to force-dynamic

--- src/app/(with_frame)/main/page.tsx
+++ src/app/(with_frame)/main/page.tsx
@@ -1,14 +1,15 @@
-import { HydrationBoundary } from '@tanstack/react-query';
 import { dehydrate } from '@tanstack/query-core';
-import { getQueryClient } from '@todo-it/core/providers/query/query-utils';
-import { auth } from '@todo-it/core/auth/auth';
-import { todoMapQueryPrefetch } from '@todo-it/core/query/todo-query';
+import { HydrationBoundary } from '@tanstack/react-query';
 import { Todo } from '@todo-it/app/(with_frame)/main/Todo';
+import { auth } from '@todo-it/core/auth/auth';
+import { getQueryClient } from '@todo-it/core/providers/query/query-utils';
+import { todoMapQueryPrefetch } from '@todo-it/core/query/todo-query';
+
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   const queryClient = getQueryClient();
   const session = await auth();
-  // todo auth 함수도 세션 에러를 캐치해서 예외처리하는 커스텀 함수로 감싸야함
   if (!session?.user?.id) {
     return null;
   }
